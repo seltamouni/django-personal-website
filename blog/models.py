95226b730b6620050990747beb657d44dd9c19fd
seltamouni <https://github.com/seltamouni>
2022-08-16 22:26:42 +0100

    design models post and about

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
+from distutils.command.upload import upload
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'post/')
+    publish_date = models.DateTimeField(default=timezone.now())
+    content = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return self.title
+    
--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,12 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    blog_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='about/')
+    subtitle =models.TextField(max_length=500)
+    aboutme = models.TextField(max_length=2000)
+    tw_link = models.URLField()
+    lk_link = models.URLField()
+    github_link = models.URLField()
